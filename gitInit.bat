4fdf52c64a276ab1c073ece5ce2e7959eb05d1c1
John Campbell <campbj49@gmail.com>
2023-07-11 17:05:14 -0400

    cleaning accidentally added file

--- gitInit.bat
+++
@@ -1,7 +0,0 @@
-git init
-git add --all
-git rm gitInit.bat
-git commit -m "first commit to GitHub"
-git branch -M main
-git remote add origin https://github.com/campbj49/Project10-FruitSearch.git
-git push -u origin main
